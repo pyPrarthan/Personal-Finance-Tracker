d16f4d8f4a206194885c19870f5b07e416b384f8
pyPrarthan <christianprarthan07@gmail.com>
2024-08-26 17:35:33 -0400

    started to work with data_entry.py

---
+++ data_entry.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def get_date(prompt, allow_default='False'):
+    pass
+
+def get_amount():
+    pass
+
+def get_category():
+    pass
+
+def get_description():
+    pass
